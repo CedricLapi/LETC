5775143dd1bc6c66cc30c43a4c1063c7bec32589
Cedric Lapi <lapicedric@gmail.com>
2024-07-04 21:59:11 +0200

    agree and meow added

---
+++ agree.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void)
+{
+char c;
+printf("Do you agree? ");
+scanf(" %c", &c);
+
+if (c == 'y' || c == 'Y')
+{
+    printf("Agreed.\n");
+}
+else if (c == 'n' || c == 'N')
+{
+    printf("disagreed.\n");
+}
+else
+{
+    printf("Invalid input.\n");
+}
+return 0;
+}
---
+++ meow.c
@@ -0,0 +1,45 @@
+/*
+#include<stdio.h>
+
+int main(void)
+{
+    int i = 3;
+    while(i > 0)//boolean expression
+    {
+        printf("meow!\n");
+        i--;
+    }
+}
+*/
+
+
+
+
+//most conventionnal way of doing it
+/*#include<stdio.h>
+
+int main(void)
+{
+    int i = 0; //start with zero
+    while (i < 3) //and less than the number you are trying to reach, so no equal sign
+    {
+        printf("meow!\n");
+        i++;
+        return 0;
+    }
+
+}
+*/
+
+
+//For loop version
+
+#include<stdio.h>
+
+int main(void)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        printf("woah!\n");
+    }
+}
