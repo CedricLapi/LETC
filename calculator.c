8dca840c316deeba55078ee9f1fe129ec5707edd
Cedric Lapi <lapicedric@gmail.com>
2024-07-06 00:47:46 +0200

    calculator

---
+++ calculator.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main(void)
+{
+    int x, y;
+    
+    printf("let x be ");
+    scanf("%i", &x);
+
+    printf("let y be ");
+    scanf("%i", &y);
+
+    printf("so the sum is: %i\n ", x + y);
+
+    return 0;
+}
