5c3b58ef0f49161317fb5354de5b605d1f34bda8
Cedric Lapi <lapicedric@gmail.com>
2024-07-06 03:41:00 +0200

    hou function commenting

--- hou.c
+++ hou.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-void hou(int n);
+void hou(int n); //hou function protype
 int main(void)
 {
     hou(10);
 }
 
-void hou(int n)
+void hou(int n) //hou function
 {
     for(int i = 0; i < n; i++)
     {
