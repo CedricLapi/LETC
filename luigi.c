3179f91f58b863b4e2f9b00d94bbd5a638b7cdb0
Cedric Lapi <lapicedric@gmail.com>
2024-07-07 07:21:33 +0200

    additor and luigi added

---
+++ luigi.c
@@ -0,0 +1,31 @@
+
+
+#include<stdio.h>
+
+void mark(int n);
+int main(void)
+
+{
+    mark(10);
+}
+
+void mark(int n)
+{
+
+    for(int i = 0; i < n; i++){
+
+
+
+    for(int j = 0; j < n; j++)
+    {
+        printf("?");
+    }
+
+
+
+    printf("\n");
+
+
+    }
+    
+}
