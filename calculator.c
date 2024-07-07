4bc7057e54f22d9b63c8aa29d71bf267702dfa49
Cedric Lapi <lapicedric@gmail.com>
2024-07-07 02:10:14 +0200

    making out scope variables in scope

--- calculator.c
+++ calculator.c
@@ -1,5 +1,7 @@
 
 #include<stdio.h>
+
+int add(int a, int b);
 int main(void)
 {
     int x, y;
@@ -10,7 +12,13 @@ int main(void)
     printf("let y be ");
     scanf("%i", &y);
 
-    printf("so the sum is: %i\n ", x + y);
+    int z = add(x, y);
+    printf("so the sum is: %i\n ", z);
 
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
