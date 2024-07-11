e757b836826430b7b183c516ea988e9f16d028d0
Cedric Lapi <lapicedric@gmail.com>
2024-07-11 18:34:47 +0200

    a program

---
+++ data.c
@@ -0,0 +1,15 @@
+
+/*  A Program that uses the characters, integers, and floating-point data-point */
+
+#include <stdio.h>
+
+int main(void){
+
+    printf("I am learning the %c programming language\n", 'c');
+    printf("i have just completed chapter %d\n", 2);
+    printf("i am %.1f percent ready to move on ", 99.9);
+    printf("to the next chapter!\n");
+
+    return 0;
+
+}
