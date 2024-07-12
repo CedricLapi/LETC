a161546b935bec1abe773675083e66a3afe4cf3b
Cedric Lapi <lapicedric@gmail.com>
2024-07-12 04:00:32 +0200

    uppercase

---
+++ uppercase.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+    char s[100];
+
+    printf("Before: ");
+    scanf("%99s", &s);
+    printf("After: ");
+    for(int i = 0, n = strlen(s); i < n; i++)
+    {
+        
+    }
+}
