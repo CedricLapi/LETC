5d61ef153275093afdfaff2d1b8bcd0d1a9ab7ee
Cedric Lapi <lapicedric@gmail.com>
2024-07-14 17:51:12 +0200

    variable

---
+++ variable.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+
+int main(void)
+
+{
+    // My variables for the program
+
+    char answer;
+    int quantity;
+    float price;
+
+    /* char first_initial;
+    char middle_initial; */
+
+    char first_initial, middle_initial;
+
+    answer = 'B';
+    quantity = 14;
+    /* price = 7.95; */
+    price = 8.50 * .65; //price after 35 percent discount
+
+    float totalAmount = price * quantity;//use value from another variable
+
+}
