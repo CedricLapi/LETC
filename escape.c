b7a50fccd00ab1c3b9c8f5eb07dc2fc4df6f047a
Cedric Lapi <lapicedric@gmail.com>
2024-07-14 02:53:15 +0200

    print command

---
+++ escape.c
@@ -0,0 +1,17 @@
+
+//Learning c
+
+#include<stdio.h>
+
+int main(void)
+{ 
+    /* These lines show how to use the most popular escape */
+
+    /* printf("Column A\t Colum B\t Column C"); //tab escape sequence
+
+    printf("\nMy computer\'s Beep sounds like this: \a"); //alarm escape sequence
+ */
+    printf("\"Letz\bs fix that typo and then show the backslash "); //beginning of the double quotation mark
+    printf("character \\\" she said\n"); //end of the quotation mark with backslash
+    printf("\\"); //backslash
+}
---
+++ print.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+int main(void){
+
+    /* printf("My favorite number is %d", 7);  // Prints My favorite number is 7
+
+    printf("\nI'm on my way to c mastery"); */
+
+    /* printf("Write code\a"); //the computer is supposed to bell
+    printf("\nLearn C"); */
+
+    printf("Write code \nlearn C programming");
+
+}
