68386bd895f78902074238fda2a24375ef3929c1
Cedric Lapi <lapicedric@gmail.com>
2024-07-15 15:35:34 +0200

    list

---
+++ list.c
@@ -0,0 +1,27 @@
+
+/* #include<stdio.h>
+#include<string.h>
+
+int main(void){
+
+    /* char month[10]; //Defines a character array */
+
+    /* char mois[] = "January"; //Declaring a character array without subscripts
+
+     strcpy (mois, "April"); //Assigning new value to the character array
+
+    printf(mois);
+
+} */ 
+
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+    char mois[] = "April";
+    strcpy (mois, "january");
+    printf(mois);
+
+}
