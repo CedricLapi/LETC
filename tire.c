438548a11f363bf62cae1915b8751d400ea9829d
Cedric Lapi <lapicedric@gmail.com>
2024-07-16 15:50:45 +0200

    netSales

--- tire.c
+++ tire.c
@@ -24,5 +24,17 @@ float tirePrice, beforeTax, netSales;
 printf("How many tires did you purchase? ");
 scanf(" %d", &numTires);
 
+printf("What was the cost per tire (enter in $XX.XX format) ? ");
+scanf(" $%f", &tirePrice);
+
+
+                /* Compute the price  */
+
+    beforeTax = tirePrice * numTires;
+    netSales = beforeTax + (beforeTax * SALESTAX);
+
+    printf("You spent $%.2f on your tires.\n\n\n", netSales);
+
+    return 0;
 
 }
