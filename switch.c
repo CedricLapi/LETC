c0a7d92e953ae892e7f8f8283d138f72c2dd3e80
Cedric Lapi <lapicedric@gmail.com>
2024-07-20 00:53:53 +0200

    switch done

--- switch.c
+++ switch.c
@@ -30,6 +30,36 @@ int main(void)
         printf ("Enter your choice: ");
         scanf (" %d", &choice);
 
+        switch (choice)
+
+        {
+            case (1):  printf("\nTo add you will need the");
+                       printf("contact's\n");
+                       printf("First name, last name, and number.\n");
+
+                       break;
+
+            case(2):   printf("\nGet ready to enter the name of the contact you wish to change.\n");
+
+                       break;
+
+            case (3):  printf("\nWhich contact do you wish to call?");
+
+                       break;
+
+            case (4): printf ("\nWhich contact do you which to text?");
+
+                       break; 
+
+            case (5): exit (1); //Exits the program early  
+
+            default: printf ("\n%d is not a valid choice.\n", choice);
+
+                     printf("Try again.\n"); 
+
+                       break;                       
+        }
+
 }
 while ( (choice < 1) || (choice > 5));
 
